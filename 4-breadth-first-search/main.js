bc74876a72e2a83daa9df335b62907fce1a12513
Joshua Martin <the.manuscriptmaster@gmail.com>
2017-02-17 19:10:36 -0600

    Main algorithm rewrite

--- 4-breadth-first-search/main.js
+++ 4-breadth-first-search/main.js
@@ -1,18 +1,21 @@
 module.exports = function bfs(graphObject, start, end) {
   var visited = [];
   var frontier = [start];
-  var cycle = 0;
-  while(frontier.length !== 0) {
-    cycle++;
-    console.log(visited);
+  var parentTree = {};
+  while (frontier.length > 0) {
     var current = frontier.shift();
-    if(current === end) {
-      console.log('Found the shortest path in ' + cycle + ' tries!');
-      return;
+    if (current === end) {
+      break;
     }
-    var newNeighbors = graphObject[current];
-    newNeighbors = newNeighbors.filter((node) => !visited.includes(node));
-    frontier.unshift(...newNeighbors);
+    var neighbors = graphObject[current];
     visited.push(current);
+    neighbors = neighbors.filter(neighbor => !visited.includes(neighbor));
+    frontier.push(...neighbors);
+    for (var neighbor of neighbors) {
+      if (!parentTree.hasOwnProperty(neighbor)) {
+        parentTree[neighbor] = current;
+      }
+    }
   }
+  
 }
